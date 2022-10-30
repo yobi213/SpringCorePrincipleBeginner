db0d5b8a0209895f7018c2ed9152feb9accb0b52
yobi <yeop7747@gmail.com>
2022-10-30 21:25:57 +0900

    AppConfig을 Spring 컨테이너로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,29 +8,45 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
-
+//
 //    public MemberService memberService() {
-//        return new MemberServiceImpl(new MemoryMemberRepository());
+//        return new MemberServiceImpl(memberRepository());
 //    }
 //
 //    public OrderService orderService() {
-//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//        return new OrderServiceImpl(memberRepository(),discountPolicy());
+//    }
+//
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//
+//    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
 //    }
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
